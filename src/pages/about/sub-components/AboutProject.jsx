aecb3cd3f7fcc35dbb129d8d1ab99ff0cfba93a9
fabiodrneles <fabiodrneles@gmail.com>
2023-02-07 10:03:49 -0300

    Criando arquivo sobr eo projeto onde busca do airtable a descricao do projeto.

---
+++ src/pages/about/sub-components/AboutProject.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect,useState } from "react";
+
+
+//import Ilustração
+import ilustração from "../../../assets/svg/about-ilustration.svg";
+
+const AboutProject = () => {
+  const [info, setInfo] = useState(null)
+
+  // Acesso api Airtable para listar as informações do projeto
+  useEffect(() => {
+      let myHeaders = new Headers();
+      myHeaders.append("Authorization", "Bearer keykXHtsEPprqdSBF");
+      myHeaders.append("Cookie", "");
+      
+      let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+      
+      fetch("https://api.airtable.com/v0/app6wQWfM6eJngkD4/Projeto?fields%5B%5D=Squad&fields%5B%5D=Sobre", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            const filtered = result.records.filter((item)  => item.id === "recGpJ5rBZs2YkRYF")
+            setInfo(filtered[0].fields.Sobre)
+          })
+        .catch(error => console.log('error', error));    
+      }, [])
+  return (
+    <div className='about-project'>
+      <main>
+        <h1 className='about-tittle'>Sobre o projeto</h1>
+        <p className='about-text'>
+        {info}
+        </p>
+      </main>
+      <img className='about-image'
+      src={ilustração} alt="Ilustração"></img>
+    </div>
+  );
+};
+
+export default AboutProject;
