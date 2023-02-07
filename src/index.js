f879b1530aba137f8b4911cd7f15087b544f182b
fabiodrneles <fabiodrneles@gmail.com>
2023-02-07 13:20:09 -0300

    Editando arquivo index. Organizando as linhas.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Login from './pages/login/Login'
+import Results from './pages/login/sub-components/List-results';
+import RoutePrivate from './pages/login/sub-components/private';
 
+import './styles/index.css'
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//Import Router//
+ReactDOM.render(
+  <BrowserRouter basename = "/">
+    <Routes>
+      <Route path="/" exact={true} element={<Home />} />
+      <Route path="/sobre" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route exact path='/results/*' element={<RoutePrivate />} >
+        <Route exact path="/results/*" element={<Results/>} />
+      </Route>
+    </Routes>
+  </ BrowserRouter>
+  ,
+  document.getElementById('root')
 );
