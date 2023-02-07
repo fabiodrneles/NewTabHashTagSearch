727f621b4ca4d58fdae4fc4b27c2c5a719923899
fabiodrneles <fabiodrneles@gmail.com>
2023-02-07 13:42:57 -0300

    Criando arquivos para descrição e organizaçao dos resultados.

---
+++ src/pages/home/sub-components/ImagesResults.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+//CSS
+import styles from './ImagesResults.module.css'
+
+const ImagesResults = ({ result }) => {
+const [className, setClassName] = useState(`${styles.imagesResults}`)
+
+    function changeClassName() {
+        if (className.includes('greatestView')) {
+            setClassName(`${styles.imagesResults}`)
+        } else {
+            setClassName(`${styles.imagesResults} ${styles.greatestView}`)
+        }
+    }
+
+    return (
+        <div 
+            className={className}
+                style={{
+                    backgroundImage: `url(${result.image_url})`
+                }}
+                onClick={changeClassName}
+                onBlur={() => alert()}
+        >
+                <div className={styles.dataPost}>
+                    <span>Postado por:</span>
+                    <span>@{result.author.username}</span>
+                </div>
+        </div>
+    )
+}
+
+export default ImagesResults;
---
+++ src/pages/home/sub-components/TweetsResults.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+//CSS
+import styles from './TweetsResults.module.css'
+
+const TweetsResults = ({ result }) => {
+
+    return (
+        <div className={styles.tweetsResults}>
+            <div className={styles.tweetsResultsContent}>
+                <div className={styles.profileImages}>
+                    <img src={result.author.profile_image_url} alt="" />
+                </div>
+                <div className={styles.tweetsContainer}>
+                    <h1>{result.author.name}<span className={styles.userName}>&nbsp;&nbsp;&nbsp;@{result.author.username}</span></h1>
+                    <p>{result.content}</p>
+                </div>
+            </div>
+            <div className={styles.linkContainer}>
+                <a href={result.url} target="_blank" rel="noopener noreferrer">Ver mais no Twitter</a>
+            </div>
+        </div>
+    )
+}
+
+export default TweetsResults;
