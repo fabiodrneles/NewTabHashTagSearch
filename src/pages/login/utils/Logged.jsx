6f0384ea4ddb9fa87782e38a4d4aae479fb275c4
fabiodrneles <fabiodrneles@gmail.com>
2023-02-07 13:15:16 -0300

    Criando um arquivo de log para localstorage.

---
+++ src/pages/login/utils/Logged.jsx
@@ -0,0 +1,21 @@
+const TOKEN_KEY = 'jwt';
+
+
+// Set Api Key Token At LocalStore \\
+export const login = () => {
+    localStorage.setItem(TOKEN_KEY, 'YOU ARE LOGGED!');
+}
+
+// Remove Api Key Token From LocalStorage \\
+export const logout = () => {
+    localStorage.removeItem(TOKEN_KEY);
+}
+
+// Check if Token Exists \\
+export const isLogin = () => {
+    if (localStorage.getItem(TOKEN_KEY)) {
+        return true;
+    }
+
+    return false;
+}
